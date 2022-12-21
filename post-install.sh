faa4f94ba6e5e8a180c6d43432221bd3b80c895f
Oriol Agost Batalla <oriolagobat@gmail.com>
2022-12-21 11:17:55 +0100

    feat(post-install): add oh my zsh plugins and verbosity

--- post-install.sh
+++ post-install.sh
@@ -1,8 +1,19 @@
 topgrade -y
 cd ./stow || exit
+
+# Delete conf files
 rm ~/.i3/config
 rm ~/.zshrc
 rm ~/.p10k.zsh
+
+#Stow
 stow -v */ -t ../../
+
+# Oh my zsh plugins
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# Copy images and wallpapers
 sudo cp ../wallpapers/* /usr/share/backgrounds/
 sudo cp ../home_images/* ~/
