d41aaf6b3f81277e63b56087e3315464b57ba23a
Oriol Agost Batalla <oriolagobat@gmail.com>
2023-01-22 18:25:02 +0100

    feat(stow/emacs): add some usefull tweaks and packages

--- stow/emacs/.emacs.d/init.el
+++ stow/emacs/.emacs.d/init.el
@@ -1,20 +1,20 @@
 (setq inhibit-startup-message t)
 
-(scroll-bar-mode -1)        ; Disable visible scrollbar
-(tool-bar-mode -1)          ; Disable the toolbar
-(tooltip-mode -1)           ; Disable tooltips
-(set-fringe-mode 10)        ; Give some breathing room
+(scroll-bar-mode -1)                                                                                    ; Disable visible scrollbar
+(tool-bar-mode -1)                                                                                      ; Disable the toolbar
+(tooltip-mode -1)                                                                                       ; Disable tooltips
+(set-fringe-mode 10)                                                                                    ; Give some breathing room
 
-(menu-bar-mode -1)            ; Disable the menu bar
+(menu-bar-mode -1)                                                                                      ; Disable the menu bar
 
 ;; Set up the visible bell
-(setq visible-bell t)
-(set-face-attribute 'default nil :font "Fira Code Retina" :height 100)
+(setq visible-bell t)                                                                                   ; Makes the screen flash on error (ex: delete when impossible, ...)
+(set-face-attribute 'default nil :font "Fira Code Retina" :height 100)                                  ; Set the font and its size
 
-(load-theme 'wombat)
+(load-theme 'wombat)                                                                                    ; Set the theme
 
 ;; Make ESC quit prompts
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)                                                 ; Makes "ESC" key quit prompts
 
 ;; Initialize package sources
 (require 'package)
@@ -39,7 +39,9 @@
          ("C-x b" . counsel-ibuffer)
          ("C-x C-f" . counsel-find-file)
          :map minibuffer-local-map
-         ("C-r" . 'counsel-minibuffer-history)))
+         ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil)) ;; Don't start searches with ^
 
 
 ;; Ivy
@@ -63,10 +65,58 @@
 
 
 ;; Doom Modeline Icons
-(use-package all-the-icons)
+(use-package all-the-icons)                                                                             ; Loads the icon package
 
 
 ;; Modeline (Doom)
 (use-package doom-modeline
   :ensure t
-  :init (doom-modeline-mode 1))
+  :init (doom-modeline-mode 1)
+  :custom ((doom-modeline-height 15)))
+
+
+;; Line numbers
+
+(column-number-mode)
+(global-display-line-numbers-mode t)          ;; To have "normal" line numbers
+;;(global-display-line-numbers-mode relative)    ;; To have relative line numbers
+
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook          ; Org mode
+                term-mode-hook         ; Term mode
+                shell-mode-hook        ; Shell mode
+                treemacs-mode-hook     ; Treemacs mode
+                eshell-mode-hook))     ; Eshell mode
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+
+;; Rainbow delimiters
+(use-package rainbow-delimiters)
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
+
+
+;; Which key
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))    ; Set delay forwhich key to pop up
+
+
+;; Ivy-rich
+(use-package ivy-rich
+  :after ivy
+  :init
+  (ivy-rich-mode 1))
+
+;; Helpful
+(use-package helpful
+  :commands (helpful-callable helpful-variable helpful-command helpful-key)
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
