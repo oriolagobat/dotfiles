24f77c6b36e4261257ed66a34d8240b633fc6aee
Oriol Agost Batalla <gpg18@agost.info>
2023-04-21 00:15:34 +0200

    nvim: tweaks on base and add keymaps

--- stow/nvim/.config/nvim/lua/oriolagobat/base.lua
+++ stow/nvim/.config/nvim/lua/oriolagobat/base.lua
@@ -7,6 +7,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 -- Don't hilight results of previous search
 vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 -- Wrap
 -- Make long lines always visible
@@ -15,14 +16,22 @@ vim.opt.wrap = true
 vim.opt.breakindent = true
 
 -- Tabs
-vim.opt.tabstop = 2
-vim.opt.softtabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
 -- Tansform tab characters to spacs
 vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 -- System wide clipboard
 vim.o.clipboard = unnamedplus
 
--- Themes
+-- Colors
 vim.o.termguicolors = true
+vim.opt.colorcolumn = "80" -- This is what draws the column on the right. Do I like it?
+
+-- Margins
+vim.opt.scrolloff = 8
+
+-- Update time
+vim.opt.updatetime = 50
--- stow/nvim/.config/nvim/lua/oriolagobat/keybindings.lua
+++ stow/nvim/.config/nvim/lua/oriolagobat/keybindings.lua
@@ -18,3 +18,30 @@ map("n", "<Leader>fs", ":wq<CR>") -- Write file and quit
 map("n", "<Leader>fd", ":qa!<CR>") -- Discard file
 map("n", "<Leader>fw", ":w<CR>") -- Write file
 map("n", "<Leader>fq", ":q<CR>") -- Quit file
+
+-- Move chunks of code when in visual mode
+map("v", "J", ":m '>+1<CR>gv=gv")
+map("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Joining lines makes cursor stay in place
+map("n", "J", "mzJ`z")
+
+-- When jumping half pages, cursor stays in the middle
+map("n", "<C-d>", "<C-d>zz")
+map("n", "<C-u>", "<C-u>zz")
+
+-- Search terms stay in the middle when searching
+map("n", "n", "nzzzv")
+map("n", "N", "Nzzzv")
+
+-- With Leader p the current copied text stays on the register
+map("x", "<leader>p", [["_dP]])
+
+-- Disable Q
+map("n", "Q", "<NOP>")
+
+-- With Leader s replace current word in all file
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- Leader x chmod's +x current file
+vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
